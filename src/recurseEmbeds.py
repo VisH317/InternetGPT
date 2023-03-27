c48936ef0fd71dc1f6d706f73f28800de3286bad
Vis <vt201916384@gmail.com>
2023-03-26 21:24:12 -0400

    finished recurseembeds setup

--- src/recurseEmbeds.py
+++ src/recurseEmbeds.py
@@ -3,16 +3,39 @@ import math
 from scraper import Scraper
 from openai.embeddings_utils import distances_from_embeddings
 from nltk.corpus import stopwords
+from typing import NamedTuple
 
+class WordEntry(NamedTuple):
+    word: str
+    tfidf: float
+
+class ScraperOutput(NamedTuple):
+    urls: list(str)
+    text: list(str)
+
+def compare_word_entry(we1: WordEntry, we2: WordEntry):
+    if we1.tfidf<we2.tfidf: return 1
+    elif we1.tfidf>we2.tfidf: return -1
+    else: return 0
 
 class RecurseEmbeds:
-    def __init__(self, subseq_cmp: int = 2, tf_idf_thresh: float = 0.5):
+    def __init__(self, subseq_cmp: int = 2, tf_idf_thresh: float = 2.0):
         self.subseq = subseq_cmp
         self.tf_idf_thresh = tf_idf_thresh
+        self.scraper = Scraper(2)
 
-    def get_subseq_embeds(self, context: str):
-        urls, text = self.scraper.query(context)
-        ctx, emb = self.get_embeds(context, text)
+    def get_subseq_embeds(self, context: str, doc: list(str)):
+        sub = self.get_word_counts(doc, context).sort(key=compare_word_entry)[:self.subseq]
+        urls_list = []
+        words_map = []
+        text_list = []
+        for s in sub:
+            urls, text = self.scraper.query(context)
+            for u in urls: urls_list.append(u)
+            for t in text: 
+                text_list.append(t)
+                words_map.append(s.word)
+        ctx, emb = self.get_embeds(context, text_list)
         distances = distances_from_embeddings(ctx, emb, distance_metric="cosine")
         vals = zip(urls, distances)
         vals = sorted(vals, key=self.sort_embeds)
@@ -27,16 +50,14 @@ class RecurseEmbeds:
         return question_embed, context_embed
     
     
-    def get_word_counts(self, doc: list(str), selected_docs: list(str)):
+    def get_word_counts(self, doc: list(str), selected_doc: str) -> list(WordEntry):
         """Gets the word counts or TF_IDF for the full document returned from the webpage, idea is that rare words based on these counts can be searched recursively in the most similar contexts"""
         # first remove stopwords and tokenize words with nltk
         # then create the TF rank - research how to represent for all documents (maybe average TF across all documents)
         # calculate IDF matrix
         # multiply and sort for highest score and search based on that as well as surrounding context words
         # instead: calculate overall idf frequency, then go through term frequency in each and determine if below a desired threshold to require searching up
-        filtered_doc = []
-        for d in selected_docs:
-            filtered_doc.append(" ".join([w for w in d.split(" ") if w.replace(",", "").replace(";", "").replace(":", "") not in stopwords.words("english")]))
+        filtered_doc = [w for w in d.split(" ") if w.replace(",", "").replace(";", "").replace(":", "") not in stopwords.words("english")]
 
         # calculate counts for IDF
         idf_cnt = {}
@@ -50,14 +71,11 @@ class RecurseEmbeds:
         for key, value in idf_cnt.items():
             idf_cnt[key] = math.log(len(doc)/value)
 
-
         doc_ctx = []
-        for d in selected_docs:
-            l = [w.replace(",", "").replace(";", "").replace(":", "") for w in d.split(" ")]
-            ret = []
-            for w in l:
-                # later take surrounding context maybe
-                if w not in ret and (l.count(w)/len(l))*idf_cnt[w]<=self.tf_idf_thresh: ret.append(w)
-            doc_ctx.append(ret)
+        ret = []
+        for w in filtered_doc:
+            # later take surrounding context maybe
+            tfidf = (filtered_doc.count(w)/len(filtered_doc))*idf_cnt[w]
+            if w not in ret and tfidf>=self.tf_idf_thresh: doc_ctx.append(WordEntry(w, tfidf))
         
         return doc_ctx
